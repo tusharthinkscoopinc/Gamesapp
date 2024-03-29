f83bf8cf6b95b5e15266bf15cc52edf815957759
Tushar Thinkscoop <tushar.makhija@thinkscoopinc.com>
2024-03-29 14:30:53 +0530

    header-done

---
+++ src/components/Landing.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+// import Hero from './Hero'
+// import Nav from './Navbar'
+// const Landing = () => {
+//   return (
+//     <>
+//     <Nav />
+//     {/* <Outlet/> */}
+//     <Hero/>
+//     </>
+//   )
+// }
+
+// export default Landing
